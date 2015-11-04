812dd87408f2e2d3a306a5da5e1a99790c52d646
William Keidel <william.keidel@gmail.com>
2015-11-05 00:52:16 +0100

    add distance functions

--- manhattan_distance.py
+++ manhattan_distance.py
@@ -1,7 +1,9 @@
 #project 1 the eight puzzle
 
-print 'Insert 8 Puzzle'
-print 'Using default puzzle'
+key_row_1 = [1,2,3]
+key_row_2 = [4,5,6]
+key_row_3 = [7,8,'b']
+key_puzzle = [key_row_1, key_row_2, key_row_3]
 
 def move_blank_left(_puzzle):
 	for row in _puzzle: 
@@ -51,14 +53,42 @@ def move_blank_down(_puzzle):
 			return
 		row_index = row_index+1
 
+def get_tile_location(_puzzle, tile):
+	row_index = 2
+	for row in _puzzle:
+		if tile in row:
+			return (row.index(tile), row_index)
+		row_index = row_index-1
+
+def get_manhattan_distance(_puzzle, _key_puzzle):
+	if _puzzle == _key_puzzle:
+		return 0
+	key_y = 2
+	distance = 0
+	for row in _puzzle:
+		key_x = 0
+		for tile in row:
+			if(tile == 'b'):
+				break
+			(x,y) = get_tile_location(_key_puzzle, tile)
+			distance += abs(key_x-x)
+			distance += abs(key_y-y)	
+			key_x+=1
+		key_y-=1	
+	return distance	
+
 
 row1 = [1,2,3]
 row2 = [4,'b',6]
 row3 = [7,5,8]
 puzzle = [row1,row2,row3]
+print puzzle
+print get_manhattan_distance(puzzle, key_puzzle)
 move_blank_up(puzzle)
 print puzzle
 move_blank_left(puzzle)
+print get_manhattan_distance(puzzle, key_puzzle)
 print puzzle
 move_blank_down(puzzle)
+print get_manhattan_distance(puzzle, key_puzzle)
 print puzzle
